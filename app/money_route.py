9fd3c8adc0b1067eeacf59fa6f85a353a50011ec
shizukani-cp <to.maimai0606@gmail.com>
2025-01-12 17:38:07 +0900

    flashのimport忘れを修正

--- app/money_route.py
+++ app/money_route.py
@@ -1,5 +1,5 @@
 import time
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from flask_login import login_required, current_user
 from models import app, db, User, Transaction, sysuser_id
 import forms
